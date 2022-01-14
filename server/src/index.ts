51997b9b016417525ae51717e2b999529323e3fc
dustin-longenecker <dl-dev@outlook.com>
2022-01-13 23:40:18 -0700

    Initialized ORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
+import { __prod__ } from './constants';
 console.log("hello world");
+import { MikroORM } from "@mikro-orm/core";
+
+
+const main = () => {
+    const orm = MikroORM.init({
+        entities: [],
+        dbName: 'lireddit',
+        user; '',
+        password: '',
+        debug: !__prod__,
+
+    });
+
+}
+main();
