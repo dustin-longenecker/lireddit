d5b01f15ae21a2fad08c10c2aeb6ae46c8266337
dustin-longenecker <dl-dev@outlook.com>
2022-01-19 14:28:25 -0700

    added migrator function to index.js for db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,32 @@
 import { __prod__ } from './constants';
 console.log("hello world");
-// import { MikroORM } from "@mikro-orm/core";
+import { MikroORM } from "@mikro-orm/core";
 import * as Post from './entities/Post';
-// import mikroOrmConfig from './mikro-orm.config';
-import { createConnection } from 'typeorm';
-import path from 'path';
+import mikroOrmConfig from './mikro-orm.config';
+// import { createConnection } from 'typeorm';
+// import path from 'path';
 
 
 const main = async () => {
-    // console.log("dirname: ", __dirname);
-    // const orm = await MikroORM.init(mikroOrmConfig);
-    // // const post = orm.em.create(Post, { title: "my first post" });
+    
+    
+    console.log("dirname: ", __dirname);
+    const orm = await MikroORM.init(mikroOrmConfig);
+    await orm.getMigrator().up();
     // const post = orm.em.create(Post, { title: "my first post" });
-    // // await orm.em.persisAndFlush(post);
-    // await orm.em.persistAndFlush(post)
-    const conn = await createConnection({
-        type: "postgres",
-        url: process.env.DATABASE_URL,
-        logging: true,
-        // synchronize: true,
-        migrations: [path.join(__dirname, "./migrations/*")],
-        entities: [Post.Post],
-    });
+    const post = orm.em.create(Post as any, { title: "my first post" });
+    // await orm.em.persisAndFlush(post);
+    await orm.em.persistAndFlush(post)
+
+
+    // const conn = await createConnection({
+    //     type: "postgres",
+    //     url: process.env.DATABASE_URL,
+    //     logging: true,
+    //     // synchronize: true,
+    //     migrations: [path.join(__dirname, "./migrations/*")],
+    //     entities: [Post.Post],
+    // });
 
 };
 
