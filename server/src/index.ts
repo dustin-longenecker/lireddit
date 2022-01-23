5567c9864a0b45b48e25e198e73502e6d4d547e4
dustin-longenecker <dl-dev@outlook.com>
2022-01-22 22:04:15 -0700

    test commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,14 +19,14 @@ const main = async () => {
     await orm.em.persistAndFlush(post)
 
 
-    // const conn = await createConnection({
-    //     type: "postgres",
-    //     url: process.env.DATABASE_URL,
-    //     logging: true,
-    //     // synchronize: true,
-    //     migrations: [path.join(__dirname, "./migrations/*")],
-    //     entities: [Post.Post],
-    // });
+    const conn = await createConnection({
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        logging: true,
+        // synchronize: true,
+        migrations: [path.join(__dirname, "./migrations/*")],
+        entities: [Post.Post],
+    });
 
 };
 
